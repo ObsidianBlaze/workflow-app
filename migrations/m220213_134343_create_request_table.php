1f66228cea84f066250701c1316a9df6f0243eef
Idada Osaigbovo James <42479950+ObsidianBlaze@users.noreply.github.com>
2022-02-13 15:12:41 +0100

    feat: finished setting up the migrations and the relationship

--- migrations/m220213_134343_create_request_table.php
+++ migrations/m220213_134343_create_request_table.php
@@ -14,13 +14,16 @@ class m220213_134343_create_request_table extends Migration
     {
         $this->createTable('{{%request}}', [
             'id' => $this->primaryKey(),
-            'status' => $this->string(20),
+            'status' => $this->string(20)->defaultValue('pending'),
             'user_id' => $this->integer(),
             'request_clearance_level' => $this->integer()->defaultValue(1),
+            'request' => $this->string(200),
             'created_at' => $this->integer(),
             'updated_at' => $this->integer()
-
         ]);
+
+        //Creating foreign key
+        $this->addForeignKey('FK_request_user_user_id','{{%request}}','user_id','{{%user}}','id');
     }
 
     /**
@@ -28,6 +31,8 @@ class m220213_134343_create_request_table extends Migration
      */
     public function safeDown()
     {
+        //Dropping foreign key
+        $this->dropForeignKey('FK_request_user_user_id','{{%request}}');
         $this->dropTable('{{%request}}');
     }
 }
