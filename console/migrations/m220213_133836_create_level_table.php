a4eaf6c174bbbcdb6212cb0e9a398abab9ae0b06
Idada Osaigbovo James <42479950+ObsidianBlaze@users.noreply.github.com>
2022-02-13 16:01:05 +0100

    fit: restructured the migrations to fit the advanced template

---
+++ console/migrations/m220213_133836_create_level_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%level}}`.
+ */
+class m220213_133836_create_level_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%level}}', [
+            'id' => $this->primaryKey(),
+            //Setting the default value of the level to 0
+            'level' => $this->integer(),
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%level}}');
+    }
+}
---
+++ console/migrations/m220213_134343_create_request_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%request}}`.
+ */
+class m220213_134343_create_request_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%request}}', [
+            'id' => $this->primaryKey(),
+            'status' => $this->string(20)->defaultValue('pending'),
+            'user_id' => $this->integer(),
+            'request_clearance_level' => $this->integer()->defaultValue(1),
+            'request' => $this->string(200),
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer()
+        ]);
+
+        //Creating foreign key
+        $this->addForeignKey('FK_request_user_user_id','{{%request}}','user_id','{{%users}}','id');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        //Dropping foreign key
+        $this->dropForeignKey('FK_request_user_user_id','{{%request}}');
+        $this->dropTable('{{%request}}');
+    }
+}
